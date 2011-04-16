7c05f519e3029da0cac5e028b85cee4bcf3a299a
Julien Richard-Foy <julien@richard-foy.fr>
2011-04-16 12:46:26 +0200

    Add installation instruction in documentation

--- project/build/GlitterProject.scala
+++ project/build/GlitterProject.scala
@@ -3,4 +3,5 @@ import de.element34.sbteclipsify._
 
 class GlitterProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
   val scalatest = "org.scalatest" % "scalatest" % "1.3"
+  override def artifactID = "glitter"
 }
