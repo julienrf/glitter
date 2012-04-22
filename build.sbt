5297ca29f5ff313a5a35939f6d6f64f149532668
Julien Richard-Foy <julien@richard-foy.fr>
2012-04-22 17:14:09 +0200

    build

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.2"
 organization := "com.github.julienrf"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+
+unmanagedClasspath in Compile += Attributed.blank(new java.io.File("yop"))
--- src/main/scala/glitter/package.scala
+++ src/main/scala/glitter/package.scala
@@ -14,7 +14,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package object glitter {
+package glitter
+
+object `package` {
 
   // Implicits
   implicit def symbolToEmptyTag(s: Symbol) = EmptyTag(s.name)
